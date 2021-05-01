691e9f6c00f5361377d5b68b91fa2faf1aa747c8
laerpeeK <847749593@qq.com>
2021-05-01 15:46:36 +0800

    ''

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" @click="itemClick(index)" :key="item" class="tab-control-item">
+      <span :class="{active: index===currentIndex}">{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex : 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    line-height:40px;
+    height: 40px;
+    position: sticky;
+    top: 44px;
+    background-color: #fff;
+  }
+
+  .tab-control-item {
+    flex:1;
+  }
+
+  .tab-control-item span {
+    padding: 5px;
+  }
+  .active {
+    color: var(--color-high-text);
+    border-bottom: 2px solid var(--color-high-text);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,11 +5,68 @@
     </nav-bar>
     <home-swiper :banner="banner" :id="id"></home-swiper>
     <home-recommend-view :recommend="recommend" :id="id"></home-recommend-view>
+    <home-feature-view></home-feature-view>
+    <tab-control :titles="['流行','新款','精选']"></tab-control>
+    <ul>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+      <li>list</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "../../components/common/navbar/NavBar";
+import TabControl from "../../components/content/tabControl/TabControl";
+
 import HomeSwiper from "./childComponents/HomeSwiper";
 import HomeRecommendView from './childComponents/HomeRecommendView'
 import HomeFeatureView from "./childComponents/HomeFeatureView";
@@ -22,7 +79,9 @@ export default {
   components:{
     NavBar,
     HomeSwiper,
-    HomeRecommendView
+    HomeRecommendView,
+    HomeFeatureView,
+    TabControl
   },
   data() {
     return {
@@ -45,8 +104,18 @@ export default {
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 1;
   }
 </style>
