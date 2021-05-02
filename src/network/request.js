72d1443fc6ba160b69aaf8aecfaf33923fbd52ec
laerpeeK <847749593@qq.com>
2021-05-02 09:13:19 +0800

    'dev'

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
     const instance = axios.create({
-        baseURL:'src',
+        baseURL:'http://152.136.185.210:7878/api/m5',
         timeout: 5000
     })
 
